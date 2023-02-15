910d363949a5a559a706946dfa53b44a80497569
xjh <934622645@qq.com>
2023-02-15 18:05:35 +0800

    resolve messy code by change the intellij jvm options

--- src/main/java/org/xjh/wechat/mp/MpApplication.java
+++ src/main/java/org/xjh/wechat/mp/MpApplication.java
@@ -1,5 +1,6 @@
 package org.xjh.wechat.mp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
  */
 @SpringBootApplication
 @ConfigurationPropertiesScan("org.xjh.wechat.mp.config")
+@Slf4j
 public class MpApplication {
 
     public static void main(String[] args) {
